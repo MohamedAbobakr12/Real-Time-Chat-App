6a3b1879a007120017362b91666bbba39d94cff2
MohamedAbobakr12 <ziadabobakr@gmail.com>
2025-10-08 21:14:43 +0300

    pages and build deployment

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,16 +1,12 @@
 import express from "express";
-import { signUp } from "../controllers/auth.controller.js";
+import { signUp, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post("/signUp", signUp);
 
-router.post("/login", (req, res) => {
-    res.send("Login EndPoint")
-});
+router.post("/login", login);
 
-router.post("/logout", (req, res) => {
-    res.send("Logout EndPoint")
-});
+router.post("/logout", logout);
 
 export default router;
