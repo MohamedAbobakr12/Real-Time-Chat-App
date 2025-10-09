73552835b01eed0e1cd0d2303c03acc5328b6289
MohamedAbobakr12 <ziadabobakr@gmail.com>
2025-10-09 11:48:33 +0300

    pages and build deployment

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { signUp, login, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
+
+router.get(arcjetProtection)
 
 router.post("/signUp", signUp);
 router.post("/login", login);
